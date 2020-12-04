d24cda242e42d6085461bddcbbceb9bfba9c4a65
adefa_ <skyout02@gmail.com>
2020-12-04 08:37:07 +0700

    frontend first

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,138 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <v-app>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+    <v-app-bar app color="primary" dark>
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-toolbar-title>UCC-Test</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="drawer = !drawer">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-card >
+      <v-navigation-drawer app v-model="drawer"   
+        right
+        width="40%"
+        color="primary"
+        >
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="name"
+              :counter="20"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="engine_dis"
+              :rules="nameRules"
+              label="Engine Display Size"
+              required
+            ></v-text-field>
+
+            <v-select
+              v-model="select"
+              :items="items"
+              :rules="[v => !!v || 'Item is required']"
+              label="Engine Display Type"
+              required
+            ></v-select>            
+            <v-text-field
+              v-model="engine_Power"
+              :rules="nameRules"
+              label="Engine Power Size"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="price"
+              :rules="nameRules"
+              label="Price"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="location"
+              :rules="nameRules"
+              label="Location"
+              required
+            ></v-text-field>
+           
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+            >
+              Validate
+            </v-btn>
+
+            <v-btn
+              color="error"
+              class="mr-4"
+              @click="reset"
+            >
+              Reset Form
+            </v-btn>
+          </v-form> 
+      </v-navigation-drawer>
+    </v-card>
+
+    <v-main>
+      <v-container fluid>
+        
+        <v-slide-x-transition>
+        <router-view></router-view>
+        </v-slide-x-transition>
+      </v-container>
+    </v-main>
+
+    <v-card>
+  <v-footer absolute app>
+    <v-card-text class="text-center">
+      &copy; {{ new Date().getFullYear() }} — <strong>UCC-Test</strong>
+    </v-card-text>
+  </v-footer>
+</v-card>
+
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: false,
+    valid: true,
+    name: '',
+    engine_dis: '',
+    engine_Power: '',
+    price: '',
+    location: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 20) || 'Name must be less than 10 characters',
+    ],    
+    select: null,
+    items: [
+      'Centimetres',
+      'Cubic Inches',
+      'Liter',      
+    ],
+    
+  }),
+  methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+}
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Unique ID
+          </th>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Engine Display
+          </th>
+          <th class="text-left">
+            Engine Power
+          </th>
+          <th class="text-left">
+            Price
+          </th>
+          <th class="text-left">
+            Location
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in vehicles"
+          :key="item.ucc_id"
+        >
+          <td>{{ item.ucc_id }}</td>
+          <td>{{ item.ucc_name }}</td>
+          <td>{{ item.ucc_engine_disp }}</td>
+          <td>{{ item.ucc_engine_pow }}</td>
+          <td>{{ item.ucc_price }}</td>
+          <td>{{ item.ucc_loc }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
+
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    vehicles: [ ],    
+  }),
+  created(){
+  console.log('get data vehicle')
+  this.axios.get('/show.php')
+    .then(res => {               
+        this.vehicles= res.data.data;
+    })
+    .catch(err => {
+        console.log(err);
+        
+    })
 }
+};
 </script>
